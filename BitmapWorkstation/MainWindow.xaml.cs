117aedd4907190b32ba551f2d1cc59a6a8ad2568
saqlz <saqlz@mail.ustc.edu.cn>
2017-09-08 13:49:06 +0800

    修改和添加Readme

--- BitmapWorkstation/MainWindow.xaml.cs
+++ BitmapWorkstation/MainWindow.xaml.cs
@@ -29,45 +29,11 @@ namespace BitmapWorkstation
 
         private void UsingBitmapData_Click(object sender, RoutedEventArgs e)
         {
-            string str = "王炎生";
-
-            Encoding GB18030Encoding = Encoding.GetEncoding(54936);
-            Encoding Windows1252Encoding = Encoding.GetEncoding(1252);
-            var byteData = GB18030Encoding.GetBytes(str);
-            string tStr = "";
-            if (byteData != null)
-            {
-                for (int i = 0; i < byteData.Length; i++)
-                {
-                    tStr += byteData[i].ToString("X2");
-                }
-            }
-            Console.WriteLine(tStr);
-
-            string GB18030EncodingStr = GB18030Encoding.GetString(byteData);
-            Console.WriteLine(GB18030EncodingStr);
-
-            string Windows1252EncodingStr = Windows1252Encoding.GetString(byteData);
-            Console.WriteLine(Windows1252EncodingStr);
-            var covertData = Encoding.UTF8.GetBytes(Windows1252EncodingStr);
-
-            var t1 = Encoding.Convert(Encoding.UTF8, Windows1252Encoding, covertData);
-            var t2 = Encoding.Convert(GB18030Encoding, Encoding.UTF8, t1);
-            var t3 = Encoding.UTF8.GetString(t2);
-            Console.WriteLine(t3);
-
-
-
-            string returnStr = "";
-            if (covertData != null)
-            {
-                for (int i = 0; i < covertData.Length; i++)
-                {
-                    returnStr += covertData[i].ToString("X2");
-                }
-            }
-            Console.WriteLine(returnStr);
-
+            var bitmap = BitmapHelpers.GeneratedBitmapByBitmapImageData("../../TestFile/TestFile.txt", 640, 480);
+            //            var bitmap = new Bitmap("../../TestFile/Image.bmp");
+            var bitmapImage = BitmapHelpers.ConvertByteArrayToBitmapImage(BitmapHelpers.ConvertBitmapToByteArray(bitmap));
+            var image = new Image() { Source = bitmapImage };
+            this.BitmapShowCanvas.Children.Add(image);
         }
     }
 }
